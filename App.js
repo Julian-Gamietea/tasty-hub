77be1dafb5f0899d9e1aa739d2b571c4a332f14d
Julian-Gamietea <julian_gamietea@hotmail.com>
2022-05-25 19:45:01 -0300

    Creating the project

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>SOY UNPEDAZO</Text>
+      <Text> amura los cantos </Text>
       <StatusBar style="auto" />
     </View>
   );
