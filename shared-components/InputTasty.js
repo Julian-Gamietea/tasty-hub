db7a3e4a317b4c792d456e3d4d6b1360bc186d70
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2022-05-28 15:02:32 -0300

    Sign In now works (with dummy dashboard)

--- shared-components/InputTasty.js
+++ shared-components/InputTasty.js
@@ -1,19 +1,34 @@
 import { View, TextInput, StyleSheet } from "react-native";
 import React from "react";
 
-export const InputTasty = (props) => {
+export const InputTasty = ({placeholder, onChange, value, passwrd}) => {
     const [text, onChangeText] = React.useState("");
-    return(
-        <View style = {styles.container}>
-            <TextInput
-                style={styles.input}
-                onChangeText={onChangeText}
-                value={text}
-                placeholder={props.placeholder}
-            />
-        </View>
-        
-    );
+    if (passwrd) {
+        return(
+            <View style = {styles.container}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChange}
+                    value={value}
+                    placeholder={placeholder}
+                    secureTextEntry
+                />
+            </View>
+            
+        );
+    } else {
+        return(
+            <View style = {styles.container}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChange}
+                    value={value}
+                    placeholder={placeholder}   
+                />
+            </View>
+            
+        );
+    }
 }
 
 const styles = StyleSheet.create(
--- sign-in/SignIn.js
+++ sign-in/SignIn.js
@@ -6,8 +6,36 @@ import image from '../assets/sign-in/login-image.png';
 import { InputTasty } from '../shared-components/InputTasty';
 import { ButtonCustom } from '../shared-components/ButtonCustom';
 import { Link } from '@react-navigation/native';
+import axios from 'axios';
+
 
 export const SignIn = ({ navigation }) => {
+
+    const [inputs, setInputs] = React.useState({
+        alias: '',
+        password: ''
+    })
+
+    const [status, setStatus] = React.useState("NO LOGIN");
+    const [userData, setUserData] = React.useState({});
+
+    const handleChange = (text, input) => {
+        setInputs({
+            ...inputs,
+            [input] : text
+        });
+    }
+
+    const login = async () => {
+            await axios.get(`https://tasty-hub.herokuapp.com/api/auth/login?alias=${inputs.alias}&password=${inputs.password}`)
+            .then (async (res) => {
+                setUserData(res.data)
+                await axios.get(`https://tasty-hub.herokuapp.com/api/user/check/registration/completion?email=${res.data.email}`)
+                .then(res => navigation.navigate('Dashboard', userData))
+                .catch( e => setStatus(e.response.data))})
+            .catch( e => setStatus(e.response.data));    
+    }
+    
     const [loaded] = useFonts({
         InterSemiBold: require ('../assets/fonts/Inter-SemiBold.ttf'),
         InterRegular: require ('../assets/fonts/Inter-Regular.ttf')
@@ -15,6 +43,8 @@ export const SignIn = ({ navigation }) => {
     if(!loaded){
         return null;
     }
+
+    
 
     return (
         
@@ -26,20 +56,26 @@ export const SignIn = ({ navigation }) => {
                         <Text style = {styles.text}> Nombre de Usuario </Text>
                         <InputTasty 
                             placeholder = 'E.g: cooking'
-                        />
+                            onChange={(text) => handleChange(text, 'alias')}
+                            value={inputs.alias}
+                            />
                     </View>
 
                     <View style = {styles.formContainerItem}>
                         <Text style = {styles.text}> Contraseña </Text>
                         <InputTasty 
                             placeholder = 'E.g: CookingMaster123'
+                            onChange={(text) => handleChange(text, 'password')}
+                            value={inputs.password}
+                            passwrd
                         />
                     </View >
                     <Link to={{screen: 'RestorePassword'}} style={styles.link} >¿Olvidaste tu contraseña?</Link>
 
                     <View style = {styles.formContainerItem2}>
-                        <ButtonCustom navigation = {navigation} mail = 'userEmail@gmail.com' screen = 'EmailSent' text = 'Iniciar Sesión'/>
+                        <ButtonCustom callback={() => login()} text = 'Iniciar Sesión'/>
                     </View>
+                    <Text>{status}</Text>
                 </View>
         </ScrollView>
     
