9795768a4cec2e2748019cc4b9c8e84069c64e73
Julian-Gamietea <julian_gamietea@hotmail.com>
2022-05-27 21:59:04 -0300

    some screens added

--- App.js
+++ App.js
@@ -6,17 +6,21 @@ import { LandingPage } from './landing-page/LandingPage';
 import { EmailSent } from './register/EmailSent';
 import { SignIn } from './sign-in/SignIn';
 import {InsertMail} from './restore-password/InsertMail'
+import { EnterData } from './register/EnterData';
+import { EnterAvatar } from './register/EnterAvatar';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+        <Stack.Navigator initialRouteName="EnterAvatar" screenOptions={{headerShown: false}}>
           <Stack.Screen name="Home" component={LandingPage}/>
           <Stack.Screen name="SignIn" component={SignIn} />
           <Stack.Screen name="Register" component={Register}/>
           <Stack.Screen name="EmailSent" component={EmailSent}/>
+          <Stack.Screen name="EnterData" component={EnterData}/>
+          <Stack.Screen name="EnterAvatar" component={EnterAvatar}/>
           <Stack.Screen name="RestorePassword" component={InsertMail}/>
           {/* habría que ir agregando las screens aca */}
         </Stack.Navigator>
