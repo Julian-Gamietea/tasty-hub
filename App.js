0b9d8457fc7fd2d2dbc630f115a7dddfc27ac92e
Julian-Gamietea <julian_gamietea@hotmail.com>
2022-05-27 22:29:28 -0300

    register success

--- App.js
+++ App.js
@@ -8,19 +8,21 @@ import { SignIn } from './sign-in/SignIn';
 import {InsertMail} from './restore-password/InsertMail'
 import { EnterData } from './register/EnterData';
 import { EnterAvatar } from './register/EnterAvatar';
+import { RegisterSuccess } from './register/RegisterSuccess';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+        <Stack.Navigator initialRouteName="RegisterSuccess" screenOptions={{headerShown: false}}>
           <Stack.Screen name="Home" component={LandingPage}/>
           <Stack.Screen name="SignIn" component={SignIn} />
           <Stack.Screen name="Register" component={Register}/>
           <Stack.Screen name="EmailSent" component={EmailSent}/>
           <Stack.Screen name="EnterData" component={EnterData}/>
           <Stack.Screen name="EnterAvatar" component={EnterAvatar}/>
+          <Stack.Screen name="RegisterSuccess" component={RegisterSuccess}/>
           <Stack.Screen name="RestorePassword" component={InsertMail}/>
           {/* habría que ir agregando las screens aca */}
         </Stack.Navigator>
