08f3e596ffab43753c9958249c6e4bbbd0a483e6
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2022-06-23 16:37:34 -0300

    Multimedia Carrousel fixed

--- carrousel-instructions/CarrouselMultimedia.js
+++ carrousel-instructions/CarrouselMultimedia.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {  Text,Image, ScrollView, StyleSheet } from "react-native";
+import { Text, Image, ScrollView, StyleSheet } from "react-native";
 import { Video } from 'expo-av';
 
 
@@ -12,42 +12,42 @@ export const CarrouselMultimedia = ({ id }) => {
 
   const video = React.useRef(null);
 
-  const MultimediaVideo= ({ item }) => {
-    return(
-      <Video 
-        source={{uri: item.urlContent}} 
+  const MultimediaVideo = ({ item }) => {
+    return (
+      <Video
+        source={{ uri: item.urlContent }}
         ref={video}
         useNativeControls
         resizeMode="contain"
         isLooping
-        style={{width:340, height: 200, marginLeft: 10, marginTop: 8, borderRadius: 10}}
+        style={{ width: 340, height: 200, marginLeft: 10, marginTop: 8, borderRadius: 10, alignSelf: 'center' }}
       />
 
     )
-    
+
   };
 
-  const MultimediaImage = ({ item,index }) => {
-        return(
-          <Image
-                key={index}
-                source={{uri: item.urlContent}}
-                style={{width:300, height: 200, marginLeft: 10, marginTop: 8, borderRadius: 10}}
-                />  
-        )
+  const MultimediaImage = ({ item, index }) => {
+    return (
+      <Image
+        key={index}
+        source={{ uri: item.urlContent }}
+        style={{ width: 300, height: 200, marginLeft: 10, marginTop: 8, borderRadius: 10, alignSelf: 'center' }}
+      />
+    )
   };
-  const RenderMultimediaItem = ({ item,index }) => {
-      if(item.typeContent.includes("image")){        
-        return (
-          <MultimediaImage item={item} key ={index}/>
-        )
-      }
-      else{
-        return(
-        <MultimediaVideo key={index} item={item}/>
-        )
-      }
-    };
+  const RenderMultimediaItem = ({ item, index }) => {
+    if (item.typeContent.includes("image")) {
+      return (
+        <MultimediaImage item={item} key={index} />
+      )
+    }
+    else {
+      return (
+        <MultimediaVideo key={index} item={item} />
+      )
+    }
+  };
 
   const fetchmultimediaInstruction = (idInstruction) => {
     const resp = axios.get(`https://tasty-hub.herokuapp.com/api/multimedia/instruction/${idInstruction}`);
@@ -61,22 +61,23 @@ export const CarrouselMultimedia = ({ id }) => {
 
   return (
 
-    <ScrollView  horizontal nestedScrollEnabled={true} style={{width: '90%'}}>
-      
-      {multimedia.map((elem,index) => {
-        if(index < multimedia.length){
-          return(
+    <ScrollView nestedScrollEnabled={true} style={{ width: '100%', maxHeight: 220 }}>
+
+      {multimedia.map((elem, index) => {
+        if (index < multimedia.length) {
+          return (
             <RenderMultimediaItem
               key={index}
               item={elem}
-              />
-          )}
-        return null
-}
-)
+            />
+          )
         }
-          
-        
+        return null
+      }
+      )
+      }
+
+
     </ScrollView>
 
   );
