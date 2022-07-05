fc1bd0de35c35ba345358e85d753de07c192d525
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2022-07-04 22:00:48 -0300

    DefineIng and Type no longer hardcoded

--- recipe-creation/DefineIngredients.js
+++ recipe-creation/DefineIngredients.js
@@ -10,126 +10,25 @@ import { Button } from "react-native-web";
 import { ButtonCustom } from "../shared-components/ButtonCustom";
 import { Dimensions } from "react-native";
 import { AntDesign } from '@expo/vector-icons';
+import axios from "axios";
 
 export const DefineIngredients = ({ navigation, route }) => {
-    const {state} = route.params;
+    const { state } = route.params;
     const [ingredientQty, setIngredientQty] = React.useState(state.ingredientQty.length > 0 ? state.ingredientQty : []);
-    const [initialLoad, setInitialLoad] = React.useState(null);
-    const ingredients = [
-        {
-            "id": 101,
-            "name": "Tomate",
-            "dividable": false
-        },
-        {
-            "id": 102,
-            "name": "Huevo",
-            "dividable": false
-        },
-        {
-            "id": 103,
-            "name": "Pollo",
-            "dividable": false
-        },
-        {
-            "id": 104,
-            "name": "Choclo",
-            "dividable": false
-        },
-        {
-            "id": 105,
-            "name": "Remolacha",
-            "dividable": false
-        },
-        {
-            "id": 106,
-            "name": "Palta",
-            "dividable": false
-        },
-        {
-            "id": 107,
-            "name": "Limon",
-            "dividable": false
-        },
-        {
-            "id": 108,
-            "name": "Aceite",
-            "dividable": false
-        },
-        {
-            "id": 109,
-            "name": "Jugo de limon",
-            "dividable": false
-        },
-        {
-            "id": 117,
-            "name": "Carne",
-            "dividable": false
-        },
-        {
-            "id": 118,
-            "name": "Pan de papa",
-            "dividable": false
-        },
-        {
-            "id": 119,
-            "name": "Queso cheddar",
-            "dividable": false
-        },
-        {
-            "id": 120,
-            "name": "Jamon",
-            "dividable": false
-        },
-        {
-            "id": 121,
-            "name": "Lechuga",
-            "dividable": false
-        }
-    ];
-
-
-    const iq =
-    {
-        ingredientId: 0,
-        unitId: 0,
-        quantity: 0,
-        recipeId: 0,
-        observations: "",
-    }
-    const units = [
-        {
-            "id": 1,
-            "description": "gramos",
-            "shortened": "gr"
-        },
-        {
-            "id": 2,
-            "description": "onzas",
-            "shortened": "oz"
-        },
-        {
-            "id": 3,
-            "description": "kilogramos",
-            "shortened": "kg"
-        },
-        {
-            "id": 4,
-            "description": "libras",
-            "shortened": "lb"
-        },
-        {
-            "id": 5,
-            "description": "litros",
-            "shortened": "lt"
-        }
-    ]
-
+    const [ingredients, setIngredients] = React.useState([]);
+    const [units, setUnits] = React.useState([]);
     const [searchValue, setSearchValue] = React.useState("");
     const [searchResults, setSearchResults] = React.useState([]);
 
     React.useEffect(() => {
-        setSearchResults(ingredients);
+        const fetchData = async () => {
+            const ing = await axios.get(`https://tasty-hub.herokuapp.com/api/ingredient`);
+            setIngredients(ing.data);
+            setSearchResults(ing.data);
+            const uni = await axios.get(`https://tasty-hub.herokuapp.com/api/unit`);
+            setUnits(uni.data);
+        }
+        fetchData();
     }, [])
 
     const handleSearch = (value) => {
@@ -148,7 +47,7 @@ export const DefineIngredients = ({ navigation, route }) => {
         return false;
     }
 
-    const handleChangeQty = (value,index) => {
+    const handleChangeQty = (value, index) => {
         const aux = ingredientQty.slice(0, ingredientQty.length);
         aux[index].quantity = value;
         setIngredientQty(aux);
@@ -162,19 +61,19 @@ export const DefineIngredients = ({ navigation, route }) => {
 
     const handlePress = () => {
         state.ingredientQty = ingredientQty;
-        navigation.navigate("RecipeForm", {state: state});
+        navigation.navigate("RecipeForm", { state: state });
     }
 
     return (
         <View style={styles.mainContainer}>
             <CustomNav text={"Agregar Ingrediente"} callback={() => navigation.goBack()} />
-            <Text style={{marginTop: 10,textAlign: 'center', fontFamily: 'InterSemiBold'}}>Busque aquí los ingredientes que desee agregar</Text>
+            <Text style={{ marginTop: 10, textAlign: 'center', fontFamily: 'InterSemiBold' }}>Busque aquí los ingredientes que desee agregar</Text>
             <View style={{ alignItems: 'center', marginTop: 10, marginBottom: 10 }}>
                 <DashboardInput placeholder={"Busque un ingrediente..."}
                     value={searchValue} onChange={(value) => handleSearch(value)}
                     onClick={() => setSearchValue("")}
                     icon="done"
-                    />
+                />
             </View>
             {searchValue !== "" ? <Text style={styles.title}>Ingredientes</Text> : <Text style={styles.title}>Ingredientes Agregados</Text>}
             <ScrollView style={styles.container}>
@@ -194,13 +93,14 @@ export const DefineIngredients = ({ navigation, route }) => {
                                                     setIngredientQty(aux);
                                                 } else {
                                                     const aux = ingredientQty.slice(0, ingredientQty.length);
-                                                    aux.push({ 
-                                                        ingredientId: ingredient.id, 
+                                                    aux.push({
+                                                        ingredientId: ingredient.id,
                                                         ingredientName: ingredient.name,
                                                         unitId: 1,
                                                         quantity: 1,
                                                         recipeId: 0,
-                                                        observations: "" });
+                                                        observations: ""
+                                                    });
                                                     console.log(aux);
                                                     setIngredientQty(aux);
                                                 }
@@ -212,11 +112,11 @@ export const DefineIngredients = ({ navigation, route }) => {
                             )
                         })}
                         {/* MENSAJE SI NO HAY RESULTADOS DE BUSQUEDA */}
-                        {searchValue !== "" && searchResults.length === 0 && 
-                        <View style={styles.emptyMessage}>
-                            <Text style={styles.text}>No tenemos el ingrediente que estas buscando...</Text>
-                            <AntDesign name="find" size={80} color="#e8e8e8" />
-                        </View>}
+                        {searchValue !== "" && searchResults.length === 0 &&
+                            <View style={styles.emptyMessage}>
+                                <Text style={styles.text}>No tenemos el ingrediente que estas buscando...</Text>
+                                <AntDesign name="find" size={80} color="#e8e8e8" />
+                            </View>}
                         {/* INGREDIENTES AGREGADOS */}
                         {searchValue === "" && ingredientQty.map((ingredient, index) => {
                             return (
@@ -236,7 +136,7 @@ export const DefineIngredients = ({ navigation, route }) => {
                                         <InputTasty
                                             keyboardType={'numeric'}
                                             maxLength={3}
-                                            style={{textAlign: 'center', minWidth: 70, maxWidth: 70 ,paddingHorizontal: 20, marginRight: 10, paddingVertical: 0, borderColor: '#000' }}
+                                            style={{ textAlign: 'center', minWidth: 70, maxWidth: 70, paddingHorizontal: 20, marginRight: 10, paddingVertical: 0, borderColor: '#000' }}
                                             value={ingredient.quantity}
                                             onChange={(value) => handleChangeQty(value, index)}
                                         />
@@ -244,7 +144,7 @@ export const DefineIngredients = ({ navigation, route }) => {
                                             <Picker
                                                 style={styles.picker}
                                                 selectedValue={ingredient.unitId}
-                                                onValueChange={(itemValue) => handleChangeUnit(itemValue, index) }>
+                                                onValueChange={(itemValue) => handleChangeUnit(itemValue, index)}>
                                                 {units.map((unit) => {
                                                     return <Picker.Item label={unit.shortened} value={unit.id} />
                                                 })}
@@ -258,11 +158,11 @@ export const DefineIngredients = ({ navigation, route }) => {
                 </View>
 
             </ScrollView>
-            <ButtonCustom 
-            text={'Aceptar'} 
-            style={{ marginHorizontal: 100, marginBottom: 15, marginTop: 10 }}
-            callback={handlePress}
-             />
+            <ButtonCustom
+                text={'Aceptar'}
+                style={{ marginHorizontal: 100, marginBottom: 15, marginTop: 10 }}
+                callback={handlePress}
+            />
         </View>
     )
 }
@@ -293,7 +193,7 @@ const styles = StyleSheet.create({
         marginBottom: 20
     },
     emptyMessage: {
-        height: Dimensions.get("screen").height*0.4,
+        height: Dimensions.get("screen").height * 0.4,
         justifyContent: 'space-around',
         alignItems: 'center',
         marginBottom: '40%'
--- recipe-creation/DefineType.js
+++ recipe-creation/DefineType.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet,ScrollView, StatusBar, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, StatusBar, Dimensions, TouchableOpacity } from 'react-native';
 import { RadioButton } from 'react-native-paper';
 import { CustomNav } from '../shared-components/CustomNav';
 import { MaterialIcons } from '@expo/vector-icons';
+import axios from 'axios';
 
 
 export const DefineType = ({ navigation, route }) => {
@@ -13,120 +14,40 @@ export const DefineType = ({ navigation, route }) => {
         description: recipe.typeDescription
     });
 
+    const [types, setTypes] = React.useState([]);
 
-    const handlePress =  () => {
+
+    const handlePress = () => {
         recipe.typeId = checked.typeId;
         recipe.typeDescription = checked.description;
         console.log(recipe);
-        navigation.navigate('RecipeForm', {state: recipe})
+        navigation.navigate('RecipeForm', { state: recipe })
     }
 
-    const types = [
-        {
-            "id": 7,
-            "description": "Americana"
-        },
-        {
-            "id": 12,
-            "description": "Aperitivo"
-        },
-        {
-            "id": 9,
-            "description": "Argentina"
-        },
-        {
-            "id": 4,
-            "description": "China"
-        },
-        {
-            "id": 26,
-            "description": "Dulces"
-        },
-        {
-            "id": 8,
-            "description": "Española"
-        },
-        {
-            "id": 18,
-            "description": "Francesa"
-        },
-        {
-            "id": 20,
-            "description": "Hamburguesa"
-        },
-        {
-            "id": 16,
-            "description": "India"
-        },
-        {
-            "id": 5,
-            "description": "Italiana"
-        },
-        {
-            "id": 13,
-            "description": "Japonesa"
-        },
-        {
-            "id": 2,
-            "description": "Mexicana"
-        },
-        {
-            "id": 27,
-            "description": "Panadería"
-        },
-        {
-            "id": 14,
-            "description": "Pasta"
-        },
-        {
-            "id": 15,
-            "description": "Peruana"
-        },
-        {
-            "id": 19,
-            "description": "Pizza"
-        },
-        {
-            "id": 11,
-            "description": "Postre"
-        },
-        {
-            "id": 1,
-            "description": "Rapida"
-        },
-        {
-            "id": 10,
-            "description": "Saludable"
-        },
-        {
-            "id": 6,
-            "description": "Suiza"
-        },
-        {
-            "id": 28,
-            "description": "Turca"
-        },
-        {
-            "id": 3,
-            "description": "Vegetariana"
+    React.useEffect(() => {
+        const fetchTypes = async () => {
+            const res = await axios.get(`https://tasty-hub.herokuapp.com/api/type`);
+            setTypes(res.data);
         }
-    ]
+        fetchTypes();
+    }, [])
+
     return (
-        <View style={{backgroundColor: '#fff', flex: 1, justifyContent: 'center', paddingTop: StatusBar.currentHeight + 5}}>
-            <CustomNav text={"Definir Tipo"} callback={() => {navigation.goBack()}}/>
-                <ScrollView style={{ backgroundColor: '#fff'}}>
-                <View style={{marginTop: 30 ,alignItems: 'flex-start', alignSelf: 'center'}}>
+        <View style={{ backgroundColor: '#fff', flex: 1, justifyContent: 'center', paddingTop: StatusBar.currentHeight + 5 }}>
+            <CustomNav text={"Definir Tipo"} callback={() => { navigation.goBack() }} />
+            <ScrollView style={{ backgroundColor: '#fff' }}>
+                <View style={{ marginTop: 30, alignItems: 'flex-start', alignSelf: 'center' }}>
                     <Text style={styles.mainText}>Seleccione la categoría a la que corresponde su plato. </Text>
                     <Text style={styles.mainText}>Tenga en cuenta que podrá seleccionar una única categoría.</Text>
                 </View>
-                <View style={{marginLeft: 20, marginTop: 50 ,alignSelf: 'center' ,flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between'}}>
+                <View style={{ marginLeft: 20, marginTop: 50, alignSelf: 'center', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
                     {types.map((type, index) => {
                         return (
                             <View key={index} style={styles.formContainerItemRadioOption}>
                                 <RadioButton
                                     value={type.id}
                                     status={checked.typeId === type.id ? 'checked' : 'unchecked'}
-                                    onPress={() => setChecked({ typeId: type.id, description: type.description})}
+                                    onPress={() => setChecked({ typeId: type.id, description: type.description })}
                                     color={"#0099FF"}
                                 />
                                 <Text style={styles.textSmall}>{type.description}</Text>
@@ -134,12 +55,12 @@ export const DefineType = ({ navigation, route }) => {
                         )
                     })}
                 </View>
-                <TouchableOpacity 
-                style={{alignSelf: 'center' ,backgroundColor: "#583209", borderRadius: 500, padding: 10, marginTop: 30, marginBottom: 20 }}
-                onPress={handlePress}
+                <TouchableOpacity
+                    style={{ alignSelf: 'center', backgroundColor: "#583209", borderRadius: 500, padding: 10, marginTop: 30, marginBottom: 20 }}
+                    onPress={handlePress}
                 >
-						<MaterialIcons name="chevron-right" size={70} color="#fff" />
-				</TouchableOpacity>
+                    <MaterialIcons name="chevron-right" size={70} color="#fff" />
+                </TouchableOpacity>
             </ScrollView>
         </View>
     )
